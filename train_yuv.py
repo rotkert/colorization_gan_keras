5b205ed9a422d1907cec79ab067aea41c3ce860f
Mikolaj Kaminski <Mikolaj.Kaminski@aitsolutions.pl>
2018-03-10 20:31:58 +0100

    poprawki

--- train_yuv.py
+++ train_yuv.py
@@ -56,64 +56,62 @@ writer.add_graph(K.get_session().graph)
 
 print("Start training")
 global_batch_counter = 1
-for e in range(1):
-#     batch_counter = 1
-#     toggle = True
-#     batch_total = data_yuv.shape[0] // BATCH_SIZE
-#     progbar = generic_utils.Progbar(batch_total * BATCH_SIZE)
-#     start = time.time()
-#     dis_res = 0
-# 
-#     while batch_counter < batch_total:
-#         uv_batch = data_uv[(batch_counter - 1) * BATCH_SIZE:batch_counter * BATCH_SIZE]
-#         y_batch = data_y[(batch_counter - 1) * BATCH_SIZE:batch_counter * BATCH_SIZE]
-# 
-#         batch_counter += 1
-# 
-#         toggle = not toggle
-#         if toggle:
-#             x_dis = np.concatenate((model_gen.predict(y_batch), y_batch), axis=3)
-#             y_dis = np.zeros((BATCH_SIZE, 1))
-#         else:
-#             x_dis = np.concatenate((uv_batch, y_batch), axis=3)
-#             y_dis = np.ones((BATCH_SIZE, 1))
-#             y_dis = np.random.uniform(low=0.9, high=1, size=BATCH_SIZE)
-# 
-#         dis_res = model_dis.train_on_batch(x_dis, y_dis)
-# 
-#         model_dis.trainable128 = False
-#         x_gen = y_batch
-#         y_gen = np.ones((BATCH_SIZE, 1))
-#         x_output = uv_batch
-#         gan_res = model_gan.train_on_batch(x_gen, [y_gen, x_output])
-#         model_dis.trainable = True
-#         
-#         progbar.add(BATCH_SIZE,
-#                     values=[("D loss", dis_res),
-#                             ("G total loss", gan_res[0]),
-#                             ("G loss", gan_res[1]),
-#                             ("G L1", gan_res[2]),
-#                             ("dis acc", gan_res[4]),
-#                             ("pacc", gan_res[7]),
-#                             ("acc", gan_res[8]),
-#                             ("mse", gan_res[9]),
-#                             ("mae", gan_res[10])])
-#  
-#         summary = utils.create_summary_batch(dis_res, gan_res)
-#         writer.add_summary(summary, global_batch_counter)
-#         global_batch_counter += 1
-#     
-#     print("")
-#     print('Epoch %s/%s, Time: %s' % (e + 1, EPOCHS, round(time.time() - start)))
-#     
+for e in range(EPOCHS):
+    batch_counter = 1
+    toggle = True
+    batch_total = data_yuv.shape[0] // BATCH_SIZE
+    progbar = generic_utils.Progbar(batch_total * BATCH_SIZE)
+    start = time.time()
+    dis_res = 0
+ 
+    while batch_counter < batch_total:
+        uv_batch = data_uv[(batch_counter - 1) * BATCH_SIZE:batch_counter * BATCH_SIZE]
+        y_batch = data_y[(batch_counter - 1) * BATCH_SIZE:batch_counter * BATCH_SIZE]
+ 
+        batch_counter += 1
+ 
+        toggle = not toggle
+        if toggle:
+            x_dis = np.concatenate((model_gen.predict(y_batch), y_batch), axis=3)
+            y_dis = np.zeros((BATCH_SIZE, 1))
+        else:
+            x_dis = np.concatenate((uv_batch, y_batch), axis=3)
+            y_dis = np.ones((BATCH_SIZE, 1))
+            y_dis = np.random.uniform(low=0.9, high=1, size=BATCH_SIZE)
+ 
+        dis_res = model_dis.train_on_batch(x_dis, y_dis)
+ 
+        model_dis.trainable128 = False
+        x_gen = y_batch
+        y_gen = np.ones((BATCH_SIZE, 1))
+        x_output = uv_batch
+        gan_res = model_gan.train_on_batch(x_gen, [y_gen, x_output])
+        model_dis.trainable = True
+         
+        progbar.add(BATCH_SIZE,
+                    values=[("D loss", dis_res),
+                            ("G total loss", gan_res[0]),
+                            ("G loss", gan_res[1]),
+                            ("G L1", gan_res[2]),
+                            ("dis acc", gan_res[4]),
+                            ("pacc", gan_res[7]),
+                            ("acc", gan_res[8]),
+                            ("mse", gan_res[9]),
+                            ("mae", gan_res[10])])
+  
+        summary = utils.create_summary_batch(dis_res, gan_res)
+        writer.add_summary(summary, global_batch_counter)
+        global_batch_counter += 1
+     
+    print("")
+    print('Epoch %s/%s, Time: %s' % (e + 1, EPOCHS, round(time.time() - start)))
+     
     if e % 5 == 0:
         image_values = []
-        for i in range (0,25):
+        for i in range (0, 100):
             y = data_test_y[i]
-            print("aaa")
             uv_pred = np.array(model_gen.predict(y[None, :, :, :]))[0]
             yuv_pred = np.r_[(y.T, uv_pred.T[:1], uv_pred.T[1:])].T
-            yuv_pred = data_test_yuv[i]
             image_value = utils.create_image_summary(yuv_pred, i)
             image_values.append(image_value)
         
