98986e4cd91a73ecc6655b1b2998f2d17050f3e6
Mikolaj Kaminski <Mikolaj.Kaminski@aitsolutions.pl>
2018-03-10 20:47:45 +0100

    logowanie przypadkow testowych

--- train_yuv.py
+++ train_yuv.py
@@ -63,13 +63,13 @@ for e in range(EPOCHS):
     progbar = generic_utils.Progbar(batch_total * BATCH_SIZE)
     start = time.time()
     dis_res = 0
- 
+  
     while batch_counter < batch_total:
         uv_batch = data_uv[(batch_counter - 1) * BATCH_SIZE:batch_counter * BATCH_SIZE]
         y_batch = data_y[(batch_counter - 1) * BATCH_SIZE:batch_counter * BATCH_SIZE]
- 
+  
         batch_counter += 1
- 
+  
         toggle = not toggle
         if toggle:
             x_dis = np.concatenate((model_gen.predict(y_batch), y_batch), axis=3)
@@ -78,16 +78,16 @@ for e in range(EPOCHS):
             x_dis = np.concatenate((uv_batch, y_batch), axis=3)
             y_dis = np.ones((BATCH_SIZE, 1))
             y_dis = np.random.uniform(low=0.9, high=1, size=BATCH_SIZE)
- 
+  
         dis_res = model_dis.train_on_batch(x_dis, y_dis)
- 
+  
         model_dis.trainable128 = False
         x_gen = y_batch
         y_gen = np.ones((BATCH_SIZE, 1))
         x_output = uv_batch
         gan_res = model_gan.train_on_batch(x_gen, [y_gen, x_output])
         model_dis.trainable = True
-         
+          
         progbar.add(BATCH_SIZE,
                     values=[("D loss", dis_res),
                             ("G total loss", gan_res[0]),
@@ -98,18 +98,39 @@ for e in range(EPOCHS):
                             ("acc", gan_res[8]),
                             ("mse", gan_res[9]),
                             ("mae", gan_res[10])])
-  
+   
         summary = utils.create_summary_batch(dis_res, gan_res)
         writer.add_summary(summary, global_batch_counter)
         global_batch_counter += 1
-     
+        
     print("")
     print('Epoch %s/%s, Time: %s' % (e + 1, EPOCHS, round(time.time() - start)))
-     
-    if e % 5 == 0:
+    
+    if (DATA_LIMIT == -1):
+        if e % 5 == 0:
+            image_values = []
+            for i in range (0, 100):
+                y = data_test_y[i]
+                uv_pred = np.array(model_gen.predict(y[None, :, :, :]))[0]
+                yuv_pred = np.r_[(y.T, uv_pred.T[:1], uv_pred.T[1:])].T
+                image_value = utils.create_image_summary(yuv_pred, i)
+                image_values.append(image_value)
+            
+            summary = tf.Summary(value = image_values)
+            writer.add_summary(summary, e)
+            
+        if e % 10 == 0:
+            ev = model_gan.evaluate(data_test_y, [np.ones((data_test_y.shape[0], 1)), data_test_uv])
+            ev = np.round(np.array(ev), 4)
+            print('G total loss: %s - G loss: %s - G L1: %s: pacc: %s - acc: %s - mse: %s - mae: %s' % (ev[0], ev[1], ev[2], ev[7], ev[8], ev[9], ev[10]))
+            summary = utils.create_summary_epoch(ev)
+            writer.add_summary(summary, e)
+            utils.save_models(RES_DIR, model_gen, model_dis, model_gan, str(e))
+        print('')
+    else:
         image_values = []
-        for i in range (0, 100):
-            y = data_test_y[i]
+        for i in range (0, 20):
+            y = data_y[i]
             uv_pred = np.array(model_gen.predict(y[None, :, :, :]))[0]
             yuv_pred = np.r_[(y.T, uv_pred.T[:1], uv_pred.T[1:])].T
             image_value = utils.create_image_summary(yuv_pred, i)
@@ -117,14 +138,4 @@ for e in range(EPOCHS):
         
         summary = tf.Summary(value = image_values)
         writer.add_summary(summary, e)
-        
-    if e % 10 == 0:
-        ev = model_gan.evaluate(data_test_y, [np.ones((data_test_y.shape[0], 1)), data_test_uv])
-        ev = np.round(np.array(ev), 4)
-        print('G total loss: %s - G loss: %s - G L1: %s: pacc: %s - acc: %s - mse: %s - mae: %s' % (ev[0], ev[1], ev[2], ev[7], ev[8], ev[9], ev[10]))
-        summary = utils.create_summary_epoch(ev)
-        writer.add_summary(summary, e)
-        utils.save_models(RES_DIR, model_gen, model_dis, model_gan, str(e))
-    print('')
-    
     
