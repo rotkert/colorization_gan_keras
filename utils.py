f88b464bfc4cdfd113adb904a7d68bb23ffedf63
Mikolaj Kaminski <Mikolaj.Kaminski@aitsolutions.pl>
2018-03-11 09:23:37 +0100

    metody do wizualizacji w oddzielnym pliku

--- utils.py
+++ utils.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 import os
-import time
+import io
 import datetime
 import argparse
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
-import io
 from skimage import color
-from scipy import misc
 from PIL import Image
 
 def init_train():
@@ -69,68 +66,3 @@ def create_image_summary(image, image_no):
     image_byte_array = image_byte_array.getvalue()
     image_summary = tf.Summary.Image(encoded_image_string = image_byte_array, height = image.shape[0], width = image.shape[1])
     return tf.Summary.Value(tag='%s/%d' % ("image", image_no), image = image_summary)
-
-def show_yuv(yuv_original, yuv_pred):
-    rgb_original = np.clip(color.yuv2rgb(yuv_original), 0, 1)
-    rgb_pred = np.clip(np.abs(color.yuv2rgb(yuv_pred)), 0, 1)
-    grey = color.rgb2grey(yuv_original)
-
-    fig = plt.figure()
-    fig.add_subplot(1, 3, 1).set_title('greyscale')
-    plt.axis('off')
-    plt.imshow(grey, cmap='gray')
-
-    fig.add_subplot(1, 3, 2).set_title('original')
-    plt.axis('off')
-    plt.imshow(rgb_original)
-
-    fig.add_subplot(1, 3, 3).set_title('gan')
-    plt.axis('off')
-    plt.imshow(rgb_pred)
-
-    plt.show()
-
-
-def show_rgb(rgb_original, rgb_pred):
-    grey = color.rgb2grey(rgb_original)
-
-    fig = plt.figure()
-    fig.add_subplot(1, 3, 1).set_title('greyscale')
-    plt.axis('off')
-    plt.imshow(grey, cmap='gray')
-
-    fig.add_subplot(1, 3, 2).set_title('original')
-    plt.axis('off')
-    plt.imshow(rgb_original)
-
-    fig.add_subplot(1, 3, 3).set_title('gan')
-    plt.axis('off')
-    plt.imshow(rgb_pred)
-
-    plt.show()
-
-
-def show_lab(lab_original, lab_pred):
-    lab_pred = lab_pred.astype(np.float64)
-    rgb_original = np.clip(color.lab2rgb(lab_original), 0, 1)
-    rgb_pred = np.clip(np.abs(color.lab2rgb(lab_pred)), 0, 1)
-    grey = color.rgb2grey(lab_original)
-
-    fig = plt.figure()
-    fig.add_subplot(1, 3, 1).set_title('greyscale')
-    plt.axis('off')
-    plt.imshow(grey, cmap='gray')
-
-    fig.add_subplot(1, 3, 2).set_title('original')
-    plt.axis('off')
-    plt.imshow(rgb_original)
-
-    fig.add_subplot(1, 3, 3).set_title('gan')
-    plt.axis('off')
-    plt.imshow(rgb_pred)
-
-    plt.show()
-    
-def save_yuv(yuv_pred, batch_no, image_no):
-    rgb_pred = np.clip(np.abs(color.yuv2rgb(yuv_pred)), 0, 1)
-    misc.imsave("F:\\magisterka\\results\\image_" + str(batch_no) + "_" + str(image_no) + ".png", rgb_pred)
