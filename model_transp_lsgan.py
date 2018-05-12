f110c78635f6e2760c58e1355bf20673a88c9b25
rotkert <rotkert@gmail.com>
2018-05-12 15:50:46 +0200

    asd

--- model_transp_lsgan.py
+++ model_transp_lsgan.py
@@ -22,7 +22,7 @@ from keras.layers.convolutional import Conv2DTranspose
 def eacc(y_true, y_pred):
     return K.mean(K.equal(K.round(y_true), K.round(y_pred)))
 
-def create_conv(filters, kernel_size, inputs, name=None, bn=True, dropout=0., strides = 1, padding='same', activation='relu'):
+def create_conv(filters, kernel_size, inputs, name=None, bn=True, dropout=0., strides = 1, padding='same', activation='relu', alpha=0.3):
     conv = Conv2D(filters, kernel_size, strides = strides, padding=padding, kernel_initializer='he_normal', name=name)(inputs)
 
     if bn:
@@ -31,14 +31,14 @@ def create_conv(filters, kernel_size, inputs, name=None, bn=True, dropout=0., st
     if activation == 'relu':
         conv = Activation(activation)(conv)
     elif activation == 'leakyrelu':
-        conv = LeakyReLU()(conv)
+        conv = LeakyReLU(alpha)(conv)
 
     if dropout != 0:
         conv = Dropout(dropout)(conv)
 
     return conv
 
-def create_conv_transpose(filters, kernel_size, inputs, name=None, bn=True, dropout=0., strides = 1, padding='same', activation='relu'):
+def create_conv_transpose(filters, kernel_size, inputs, name=None, bn=True, dropout=0., strides = 1, padding='same', activation='relu', alpha=0.3):
     conv = Conv2DTranspose(filters, kernel_size, strides = strides, padding=padding, kernel_initializer='he_normal', name=name)(inputs)
 
     if bn:
@@ -47,7 +47,7 @@ def create_conv_transpose(filters, kernel_size, inputs, name=None, bn=True, drop
     if activation == 'relu':
         conv = Activation(activation)(conv)
     elif activation == 'leakyrelu':
-        conv = LeakyReLU()(conv)
+        conv = LeakyReLU(alpha)(conv)
 
     if dropout != 0:
         conv = Dropout(dropout)(conv)
@@ -57,21 +57,21 @@ def create_conv_transpose(filters, kernel_size, inputs, name=None, bn=True, drop
 
 def create_model_gen(input_shape, output_channels):
     inputs = Input(input_shape)
-    conv1 = create_conv(64, (4, 4), inputs, 'conv1', strides = 2, activation='leakyrelu')
-    conv2 = create_conv(128, (4, 4), conv1, 'conv2', strides = 2, activation='leakyrelu')
-    conv3 = create_conv(256, (4, 4), conv2, 'conv3', strides = 2, activation='leakyrelu')
-    conv4 = create_conv(512, (4, 4), conv3, 'conv4', strides = 2, activation='leakyrelu')
-    conv5 = create_conv(512, (4, 4), conv4, 'conv5', strides = 2, activation='leakyrelu')
+    conv1 = create_conv(64, (4, 4), inputs, 'conv1', strides = 2, activation='leakyrelu', alpha=0.2)
+    conv2 = create_conv(128, (4, 4), conv1, 'conv2', strides = 2, activation='leakyrelu', alpha=0.2)
+    conv3 = create_conv(256, (4, 4), conv2, 'conv3', strides = 2, activation='leakyrelu', alpha=0.2)
+    conv4 = create_conv(512, (4, 4), conv3, 'conv4', strides = 2, activation='leakyrelu', alpha=0.2)
+    conv5 = create_conv(512, (4, 4), conv4, 'conv5', strides = 2, activation='leakyrelu', alpha=0.2)
     
-    conv6 = create_conv_transpose(512, (4, 4), conv5, 'conv6', strides = 2, activation='leakyrelu')
+    conv6 = create_conv_transpose(512, (4, 4), conv5, 'conv6', strides = 2, activation='relu', dropout=0.5)
     merge6 = concatenate([conv6, conv4], axis=3)
-    conv7 = create_conv_transpose(512, (4, 4), merge6, 'conv7', strides = 2, activation='leakyrelu')
+    conv7 = create_conv_transpose(512, (4, 4), merge6, 'conv7', strides = 2, activation='relu', dropout=0.5)
     merge7 = concatenate([conv7, conv3], axis=3)
-    conv8 = create_conv_transpose(256, (4, 4), merge7, 'conv8', strides = 2, activation='leakyrelu')
+    conv8 = create_conv_transpose(256, (4, 4), merge7, 'conv8', strides = 2, activation='relu', dropout=0.5)
     merge8 = concatenate([conv8, conv2], axis=3)
-    conv9 = create_conv_transpose(128, (4, 4), merge8, 'conv9', strides = 2, activation='leakyrelu')
+    conv9 = create_conv_transpose(128, (4, 4), merge8, 'conv9', strides = 2, activation='relu')
     merge9 = concatenate([conv9, conv1], axis=3)
-    conv10 = create_conv_transpose(64, (4, 4), merge9, 'conv10', strides = 2, activation='leakyrelu')
+    conv10 = create_conv_transpose(64, (4, 4), merge9, 'conv10', strides = 2, activation='relu')
     
     conv11 = Conv2D(output_channels, (1, 1), padding='same', name='conv11', activation = 'tanh')(conv10)
 
@@ -82,19 +82,19 @@ def create_model_gen(input_shape, output_channels):
 
 def create_model_dis(input_shape):
     inputs = Input(input_shape)
-    conv1 = create_conv(64, (3, 3), inputs, 'conv1', activation='leakyrelu', dropout=.8)
+    conv1 = create_conv(64, (3, 3), inputs, 'conv1', activation='leakyrelu', dropout=.5, alpha=0.2)
     pool1 = MaxPool2D((2, 2))(conv1)
 
-    conv2 = create_conv(128, (3, 3), pool1, 'conv2', activation='leakyrelu', dropout=.8)
+    conv2 = create_conv(128, (3, 3), pool1, 'conv2', activation='leakyrelu', dropout=.5, alpha=0.2)
     pool2 = MaxPool2D((2, 2))(conv2)
 
-    conv3 = create_conv(256, (3, 3), pool2, 'conv3', activation='leakyrelu', dropout=.8)
+    conv3 = create_conv(256, (3, 3), pool2, 'conv3', activation='leakyrelu', dropout=.5, alpha=0.2)
     pool3 = MaxPool2D((2, 2))(conv3)
 
-    conv4 = create_conv(512, (3, 3), pool3, 'conv4', activation='leakyrelu', dropout=.8)
+    conv4 = create_conv(512, (3, 3), pool3, 'conv4', activation='leakyrelu', dropout=.5, alpha=0.2)
     pool4 = MaxPool2D((2, 2))(conv4)
 
-    conv5 = create_conv(512, (3, 3), pool4, 'conv5', activation='leakyrelu', dropout=.8)
+    conv5 = create_conv(512, (3, 3), pool4, 'conv5', activation='leakyrelu', dropout=.5, alpha=0.2)
 
     flat = Flatten()(conv5)
     dense6 = Dense(1, activation='linear')(flat)
